b4637f10c8d69bd8445385df4dbe3a7faa764d28
Temuudei Shiilegdamba <temuudei_08@yahoo.com>
2023-04-03 09:35:56 -0700

    Exercise on large class smell is done

---
+++ M2/Exercise-large-class-smell/exercise-large-class-smell/src/main/java/org/example/Student.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private double GPA;
+    private Teacher teacher;
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+}
---
+++ M2/Exercise-large-class-smell/exercise-large-class-smell/src/main/java/org/example/Teacher.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Teacher {
+    private String homeroomTeacherFirstName;
+    private String homeroomTeacherLastName;
+    public String getHomeroomTeacherLastName() {
+        return homeroomTeacherLastName;
+    }
+
+    public void setHomeroomTeacherLastName(String homeroomTeacherLastName) {
+        this.homeroomTeacherLastName = homeroomTeacherLastName;
+    }
+
+    public String getHomeroomTeacherFirstName() {
+        return homeroomTeacherFirstName;
+    }
+
+    public void setHomeroomTeacherFirstName(String homeroomTeacherFirstName) {
+        this.homeroomTeacherFirstName = homeroomTeacherFirstName;
+    }
+}
