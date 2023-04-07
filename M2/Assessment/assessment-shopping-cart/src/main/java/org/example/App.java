f4f0af83ef9850bce3cc483d562bfc96819f0920
Temuudei Shiilegdamba <temuudei_08@yahoo.com>
2023-04-07 09:31:09 -0700

    Added App class

--- M2/Assessment/assessment-shopping-cart/src/main/java/org/example/App.java
+++ M2/Assessment/assessment-shopping-cart/src/main/java/org/example/App.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.controller.ProductController;
+import org.example.service.ProductManager;
+import org.example.service.ProductManagerImpl;
+import org.example.view.ConsoleIO;
+import org.example.view.ConsoleIOImpl;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ConsoleIO io = new ConsoleIOImpl();
+        ProductManager productManager = new ProductManagerImpl();
+        ProductController productController = new ProductController(io, productManager);
+        productController.run();
     }
 }
--- M2/Assessment/assessment-shopping-cart/src/main/java/org/example/controller/ProductController.java
+++ M2/Assessment/assessment-shopping-cart/src/main/java/org/example/controller/ProductController.java
@@ -1,4 +1,19 @@
 package org.example.controller;
 
+import org.example.service.ProductManager;
+import org.example.view.ConsoleIO;
+
 public class ProductController {
+    private ConsoleIO io;
+    private ProductManager productManager;
+
+    public ProductController(ConsoleIO io, ProductManager productManager) {
+        this.io = io;
+        this.productManager = productManager;
+    }
+
+    public void run() {
+        boolean keepRunning = true;
+        io.displayMessage("Main Menu");
+    }
 }
