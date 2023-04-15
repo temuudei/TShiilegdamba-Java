cefafab2088cd80a9835f151175b103a56b42c7f
Temuudei Shiilegdamba <temuudei_08@yahoo.com>
2023-04-14 21:22:17 -0700

    Assessment not done yet; most implementations are done

---
+++ M3/assessment/sustainable-foraging/src/main/java/learn/foraging/domain/ForagerService.java
@@ -0,0 +1,77 @@
+package learn.foraging.domain;
+
+import learn.foraging.data.DataException;
+import learn.foraging.data.ForagerRepository;
+import learn.foraging.models.Forage;
+import learn.foraging.models.Forager;
+import org.springframework.stereotype.Service;
+
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.stream.Collectors;
+@Service
+public class ForagerService {
+
+    private final ForagerRepository repository;
+
+    public ForagerService(ForagerRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Forager> findByState(String stateAbbr) {
+        return repository.findByState(stateAbbr);
+    }
+
+    public List<Forager> findByLastName(String prefix) {
+        return repository.findAll().stream()
+                         .filter(i -> i.getLastName().startsWith(prefix))
+                         .collect(Collectors.toList());
+    }
+
+    public Result<Forager> add(Forager forager) throws DataException {
+        Result<Forager> result = validate(forager);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        result.setPayload(repository.add(forager));
+        return result;
+    }
+
+    private Result<Forager> validate(Forager forager) {
+        Result<Forager> result = validateNulls(forager);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        checkDuplicate(forager, result);
+        return result;
+    }
+
+    private Result<Forager> validateNulls(Forager forager) {
+        Result<Forager> result = new Result<>();
+        if (forager == null) {
+            result.addErrorMessage("Forager must not be null");
+            return result;
+        }
+        if (forager.getFirstName() == null || forager.getFirstName().isBlank()) {
+            result.addErrorMessage("Forager's first name is required");
+        }
+        if (forager.getLastName() == null || forager.getFirstName().isBlank()) {
+            result.addErrorMessage("Forager's last name name is required");
+        }
+        if (forager.getState() == null) {
+            result.addErrorMessage("Forager's state is required");
+        }
+        return result;
+    }
+
+    private void checkDuplicate(Forager forager, Result<Forager> result) {
+        if (repository.findAll().stream()
+                .anyMatch(f -> f.getFirstName().equalsIgnoreCase(forager.getFirstName())
+                            && f.getLastName().equalsIgnoreCase(forager.getLastName())
+                            && f.getState().equalsIgnoreCase(forager.getState()))) {
+            result.addErrorMessage("" + forager.getFirstName() + " "
+                                      + forager.getLastName() + ", "
+                                      + forager.getState() + " is already in the file. No duplicates are allowed.");
+        }
+    }
+}
---
+++ M3/assessment/sustainable-foraging/src/main/java/learn/foraging/domain/ItemService.java
@@ -0,0 +1,85 @@
+package learn.foraging.domain;
+
+import learn.foraging.data.DataException;
+import learn.foraging.data.ItemRepository;
+import learn.foraging.models.Category;
+import learn.foraging.models.Item;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+@Service
+public class ItemService {
+
+    private final ItemRepository repository;
+
+    public ItemService(ItemRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Item> findByCategory(Category category) {
+        return repository.findAll().stream()
+                         .filter(i -> i.getCategory() == category)
+                         .collect(Collectors.toList());
+    }
+
+    public Result<Item> add(Item item) throws DataException {
+        Result<Item> result = validate(item);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        result.setPayload(repository.add(item));
+        return result;
+    }
+
+    private Result<Item> validate(Item item) {
+        Result<Item> result = validateNulls(item);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        checkDuplicate(item, result);
+        validatePrice(item, result);
+        validateCategoryItemPrice(item, result);
+        return result;
+    }
+
+    private Result<Item> validateNulls(Item item) {
+        Result<Item> result = new Result<>();
+        if (item == null) {
+            result.addErrorMessage("Item must not be null");
+            return result;
+        }
+
+        if (item.getName() == null || item.getName().isBlank()) {
+            result.addErrorMessage("Item name is required.");
+        }
+
+        if (item.getDollarPerKilogram() == null) {
+            result.addErrorMessage("$/Kg is required.");
+        }
+        return result;
+    }
+
+    private void checkDuplicate(Item item, Result<Item> result) {
+        if (repository.findAll().stream()
+                                .anyMatch(i -> i.getName().equalsIgnoreCase(item.getName()))) {
+            result.addErrorMessage(String.format("Item '%s' is a duplicate.", item.getName()));
+        }
+    }
+
+    private void validatePrice(Item item, Result<Item> result) {
+        if ((item.getDollarPerKilogram().compareTo(BigDecimal.ZERO) < 0
+                || item.getDollarPerKilogram().compareTo(new BigDecimal("7500.00")) > 0)) {
+            result.addErrorMessage("%/Kg must be between 0.00 and 7500.00.");
+        }
+    }
+
+    private void validateCategoryItemPrice(Item item, Result<Item> result) {
+        if (item.getCategory().equals(Category.valueOf("INEDIBLE")) || item.getCategory().equals(Category.valueOf("POISONOUS"))) {
+            if (item.getDollarPerKilogram().compareTo(BigDecimal.ZERO) > 0) {
+                result.addErrorMessage("POISONOUS and INEDIBLE categories items' prices cannot exceed $0.");
+            }
+        }
+    }
+}
