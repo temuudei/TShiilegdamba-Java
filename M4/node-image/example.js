c755cb90931b73a89748f5fb17b09629cb42f740
Temuudei Shiilegdamba <temuudei_08@yahoo.com>
2023-04-24 08:46:53 -0700

    added node ex

---
+++ M4/node-image/example.js
@@ -0,0 +1,6 @@
+// Print a message to standard out.
+console.log("Hello from Node.js inside Docker.");
+// Print the current time.
+const now = new Date();
+console.log("The current time is - %s:%s:%s",
+    now.getHours(), now.getMinutes(), now.getSeconds());
